1532d862168baad1ac6dbf86ab07b81010f1f8f6
chitra-k-e <chitrake.21msc@kongu.edu>
2025-01-06 09:30:21 +0530

    Frontend and backend connected

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App'
+// import App from './App'
+import Myroot from './Myroot'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App/>
+    <Myroot />
   </StrictMode>,
 )
